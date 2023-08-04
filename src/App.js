c1115947a45f632cfa674c3c9d6e0bbcc2c64897
simonsajjizoft <simon.sajimathews@zoftsolutions.com>
2023-08-04 12:27:00 +0530

    React JSX1 Assignment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About Us</h1>
+      <p>At Zappos.com, our purpose is Simple : to live and deliver WOW.</p>
+      <p>Twenty years ago we began as a small retailer that only sold shoes. Today we still sell shoes - as well as <br></br> clothing, handbags, accessories and more. The "more" is providing the best customer service, customer experience and <br/>company culture. We are showing the world that it is possible to simultaneously deliver<br/> happiness to  the customers, employees, vendors and community in long term sustainable way.</p>
+      <p>We hope that in the future people wont even realize that we started selling shoes online. Instead they will know Zappos as a <br/>service company that  sells ____</p>    
+    </div>
+  );
+}
+
+export default App;
